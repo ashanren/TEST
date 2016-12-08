8ac00aeb923425a4682a3e1da8e03542b6d230e3
undefined <undefined>
2016-12-08 04:56:05 +0000

    dd

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ public class test
 	public static void main(String[] args)
 	{
 		// Edit this class as you please
-		System.out.println("Hello Worl!");
+		System.out.println("Hello Worldd!");
 	}
 }
